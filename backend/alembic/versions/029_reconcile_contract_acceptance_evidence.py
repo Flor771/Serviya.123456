9e80e6edc0b46afbebbc43339fe7f765f9c2351f
Flor771 <moraleselepoder@gmail.com>
2026-09-12 12:22:19 -0400

    fix(migrations): shorten acceptance revision id

--- backend/alembic/versions/029_reconcile_contract_acceptance_evidence.py
+++ backend/alembic/versions/029_reconcile_contract_acceptance_evidence.py
@@ -3,7 +3,7 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-revision: str = "029_reconcile_contract_acceptance"
+revision: str = "029_reconcile_acceptance"
 down_revision: Union[str, None] = "028_clean_test_data"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
